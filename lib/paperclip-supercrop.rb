670bd66b52ef9e29e03aace5b1d8462817d0e595
teejayvanslyke <teejay.vanslyke@gmail.com>
2010-03-11 12:14:33 -0800

    First iteration with gravity.  Going to test in a real app now.

--- lib/paperclip-supercrop.rb
+++ lib/paperclip-supercrop.rb
@@ -1,6 +1,79 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-module PaperclipSupercrop
-  VERSION = '0.0.1'
+require 'rubygems'
+require 'paperclip'
+require 'rmagick'
+
+module Paperclip
+  class Supercrop < Processor
+
+    VERSION = '0.0.1'
+
+    def initialize(file, options={})
+      @file    = file
+      @options = options
+    end
+
+    def make
+      reset_file_position
+
+      image_to_file(
+        image.crop_resized(width, height, gravity)
+      )
+    end
+
+    protected
+
+    def width
+      @options[:width] || image.columns
+    end
+
+    def height 
+      @options[:height] || image.rows
+    end
+
+    def gravity
+      case @options[:gravity]
+      when :forget
+        Magick::ForgetGravity
+      when :northwest
+        Magick::NorthWestGravity
+      when :north
+        Magick::NorthGravity
+      when :northeast
+        Magick::NorthEastGravity
+      when :west
+        Magick::WestGravity
+      when :center
+        Magick::CenterGravity
+      when :east
+        Magick::EastGravity
+      when :southwest
+        Magick::SouthGravity
+      when :southeast
+        Magick::SouthEastGravity
+      else
+        Magick::CenterGravity
+      end
+    end
+
+    def image
+      Magick::Image.read(@file).first
+    end
+
+    def basename
+      File.basename(@file.path)
+    end
+
+    def image_to_file(image)
+      file = Tempfile.new([ basename, 'png' ].compact.join('.'))
+      image.write('png:' + file.path)
+      return file
+    end
+
+    def reset_file_position
+      @file.pos = 0
+    end
+  end
 end
--- spec/paperclip-supercrop_spec.rb
+++ spec/paperclip-supercrop_spec.rb
@@ -1,11 +1,57 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
+describe Paperclip::Supercrop do
   
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+  def image(name)
+    File.open(File.dirname(__FILE__) + '/images/' + name + '.png')
   end
-  
+
+  def rmagick_image(file)
+    Magick::Image.read(file).first
+  end
+
+  def process(image_name, options={})
+    supercrop = Paperclip::Supercrop.new(image(image_name), options)
+    Magick::Image.read(supercrop.make.path).first
+  end
+
+  it 'should be a Paperclip::Processor' do
+    Paperclip::Supercrop.superclass.should == Paperclip::Processor
+  end
+
+  describe '- When processing an image with the same aspect ratio as the processor' do
+
+    before { @image = process('100x100', :width => 50, :height => 50) }
+
+    it 'should resize the image to the new dimensions' do
+      @image.columns.should == 50
+      @image.rows.should == 50
+    end
+  end
+
+  describe '- When processing an image with a different aspect ratio from the processor' do
+
+    context 'and no gravity is specified' do
+      before { @image = process('100x100', :width => 50, :height => 100) }
+
+      it 'should resize the image to the new dimensions and clip the edges evenly' do
+        @image.columns.should == 50
+        @image.rows.should    == 100
+      end
+    end
+
+    context 'and north gravity is specified' do
+      before :each do
+        @image = process('100x100', :width => 100, :height => 50, :gravity => :north)
+      end
+
+      it 'should resize the image to the new dimensions and clip the edges evenly' do
+        @image.columns.should == 100 
+        @image.rows.should    == 50
+      end
+    end
+
+  end
+
 end
+
